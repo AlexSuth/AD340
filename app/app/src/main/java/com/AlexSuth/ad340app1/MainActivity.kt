3d85fa79fa856a78e2e0d237eed1d558d40d8552
Alexander Sutherland <alexandernsutherland@gmail.com>
2020-04-28 16:29:10 -0700

    Created assignment 2. Working text edit with submit button. Displays zipcode if input is 5 characters, and throws different errors for too long and too short. Did some color customization and messed with dimensions as well.

--- app/app/src/main/java/com/AlexSuth/ad340app1/MainActivity.kt
+++ app/app/src/main/java/com/AlexSuth/ad340app1/MainActivity.kt
@@ -2,11 +2,31 @@ package com.AlexSuth.ad340app1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zipcodeEditText: EditText = findViewById(R.id.zipcodeEditText)
+        val enterButton: Button = findViewById(R.id.enterButton)
+
+        enterButton.setOnClickListener {
+            val zipcode: String = zipcodeEditText.text.toString()
+
+            if (zipcode.length >5){
+                Toast.makeText(this, R.string.zip_error_long, Toast.LENGTH_SHORT).show()
+            }else if (zipcode.length <5){
+                Toast.makeText(this, R.string.zip_error_short, Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(this, "Thanks! Your zipcode is: " + zipcode , Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
+
 }
